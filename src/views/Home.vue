580b5a4b54bef21cf7e5e54b0f7567095e257eaf
Michael_Pang <wengwengpang@gmail.com>
2021-07-08 10:17:56 +0800

    initial Draft 2021-07-08

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,7 @@
               :src="require(`@/assets/${destination.image}`)"
               alt="destination.name"
             />
+            s
           </router-link>
         </figure>
       </div>
